e24191c95f686212e03fb69105e9c2e61f6c07a8
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-10-20 18:49:28 +0400

    tests: Write initial Unit Tests

--- src/components/__tests__/PrevueMD.spec.ts
+++ src/components/__tests__/PrevueMD.spec.ts
@@ -3,9 +3,12 @@ import { describe, it, expect } from "vitest"
 import { mount } from "@vue/test-utils"
 import PrevueMD from "../PrevueMD.vue"
 
-describe("Header contains correct Title", () => {
-  it("renders properly", () => {
+describe("PrevueMD Component", () => {
+  it("Contains an correct h1 tag", () => {
     const wrapper = mount(PrevueMD)
-    expect(wrapper.html()).toContain("PrevueMD")
+    const h1 = wrapper.find("h1")
+
+    expect(h1.exists()).toBe(true)
+    expect(h1.text()).toBe("PrevueMD")
   })
 })
