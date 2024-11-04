749cc0edec5ae0547f2d9b26a0cd0b03acf4fbba
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-04 20:56:02 +0400

    test: Remove outdated MarkdownEditor and MarkdownPreview tests; enhance PrevueMD tests with clearer descriptions and additional structure

--- src/components/__tests__/PrevueMD.spec.ts
+++ src/components/__tests__/PrevueMD.spec.ts
@@ -6,14 +6,14 @@ import PrevueMD from "../PrevueMD.vue";
 describe("PrevueMD Component", () => {
   const WRAPPER = mount(PrevueMD);
 
-  it("Should have an h1 tag with the text PrevueMD", () => {
+  it("Should contain a h1 tag with the text PrevueMD", () => {
     const h1 = WRAPPER.find("h1");
 
     expect(h1.exists()).toBe(true);
     expect(h1.text()).toBe("PrevueMD");
   });
 
-  it("Should contain button that opens README from github", () => {
+  it("Should contain a button that opens README from github", () => {
     const button = WRAPPER.find("button");
     const buttonText = button.text();
 
@@ -21,7 +21,7 @@ describe("PrevueMD Component", () => {
     expect(button.exists()).toBe(true);
   });
 
-  it("Should have a link to the repo in the footer", () => {
+  it("Should contain a link to the repo in the footer", () => {
     const footer = WRAPPER.find("footer");
     const repoLinkUrl = footer.find("a").attributes("href");
 
@@ -29,11 +29,32 @@ describe("PrevueMD Component", () => {
     expect(repoLinkUrl).toBe("https://github.com/wazeerc/PrevueMD");
   });
 
-  it("Should contain edit and preview elements", () => {
+  it("Should contain the edit and preview elements", () => {
     const editor = WRAPPER.find("textarea");
     const preview = WRAPPER.find("div");
 
     expect(editor.exists()).toBe(true);
     expect(preview.exists()).toBe(true);
+  });
+});
+
+describe("PrevueMD Component: Editor", () => {
+  const WRAPPER = mount(PrevueMD);
+
+  it("Should contain a non-resizable text area", () => {
+    const textArea = WRAPPER.find("textarea");
+
+    expect(textArea.exists()).toBe(true);
+    expect(textArea.classes()).toContain("resize-none");
+  });
+});
+
+describe("PrevueMD Component: Preview", () => {
+  const WRAPPER = mount(PrevueMD);
+
+  it("Should contain a code element", () => {
+    const code = WRAPPER.find("code");
+
+    expect(code.exists()).toBe(true);
   });
 });
