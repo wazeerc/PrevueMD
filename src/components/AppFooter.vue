c96d8716c0e7dfd9bf1dab19a97a3efb58878138
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-06 20:17:30 +0400

    feat: Update AppFooter animations & Add Vue icon

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,18 +4,22 @@ import cn from "../utils/lib"
 
 <template>
   <footer class="header-footer border-t-2">
-    <p :class="cn('font-sans font-normal text-neutral-500', 'text-xs', 'sm:text-sm', 'md:text-xs', 
-      'motion-preset-rebound-right motion-delay-200')">
+    <p :class="cn('flex items-center font-sans font-normal text-neutral-500', 'text-xs',
+    'sm:text-sm', 'md:text-xs',
+    'motion-preset-fade-lg motion-delay-200',
+    )">
       Built with Vue
+      <a href="https://vuejs.org" target="_blank">
+        <img src="/vue.svg" alt="Vue Icon" :class="cn('ml-2 h-6 w-6', 'hover:motion-preset-shake')" />
+      </a>
     </p>
     <a href="https://github.com/wazeerc/PrevueMD" target="_blank" :class="cn(
       'font-sans font-normal text-neutral-500',
       'text-xs',
       'sm:text-sm',
       'md:text-xs',
-      'underline',
-      'motion-preset-rebound-left motion-delay-200',
-      'hover:text-neutral-400 transition-colors ease duration-200',
+      'motion-preset-fade-lg motion-delay-200',
+      'hover:underline hover:text-neutral-400 transition-colors ease duration-200',
     )">Collaborate here!</a>
   </footer>
 </template>
