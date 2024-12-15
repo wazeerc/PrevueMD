946cbce79d1ca47a3bf6b756c9092220110052c1
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-12-15 11:21:19 +0400

    fix(lib): Update parseMarkdown return type to VFile

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -14,6 +14,7 @@ export function cn(...inputs: ClassValue[]): string {
 //#endregion
 
 //#region: Markdown parsing utils
+import type { VFile } from "node_modules/rehype-raw/lib";
 import rehypeFormat from "rehype-format";
 import rehypeRaw from 'rehype-raw';
 import rehypeStringify from 'rehype-stringify';
@@ -38,7 +39,7 @@ import { unified } from 'unified';
  *
  * The function allows dangerous HTML through the remarkRehype configuration.
  */
-export async function parseMarkdown(textToParseIntoMarkdown: string): Promise<unknown> {
+export async function parseMarkdown(textToParseIntoMarkdown: string): Promise<VFile> {
   const markdownProcessor = unified()
     .use(remarkParse)
     .use(remarkGfm)
