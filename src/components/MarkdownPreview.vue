a3abe457fcaebe29e5cc180c12c8a7e166eb1504
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-11-26 20:35:34 +0400

    fix: add focus styles for prevuemd-modal

--- src/components/MarkdownPreview.vue
+++ src/components/MarkdownPreview.vue
@@ -106,7 +106,7 @@ watch(() => props.scrollPercentage, syncScroll);
            aria-modal="true"
            aria-labelledby="modal-title"
            tabindex="-1"
-           class="fixed inset-0 z-50 flex items-center justify-center bg-neutral-500/40 dark:bg-neutral-950/50 backdrop-blur-md
+           class="prevuemd-modal fixed inset-0 z-50 flex items-center justify-center bg-neutral-500/40 dark:bg-neutral-950/50 backdrop-blur-md
            motion-preset-focus motion-duration-500"
            @click.self="closeModal">
         <div
